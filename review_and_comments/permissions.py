97fa5ec60d095efa8d896095f2e7a796dec54c1a
AlekseiSandakov <axe455990@mail.ru>
2021-07-26 23:00:06 +0700

    my 4 commit

--- review_and_comments/permissions.py
+++ review_and_comments/permissions.py
@@ -5,7 +5,7 @@ class ReviewCommentPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.user.is_authenticated:
+        elif request.user.is_authenticated:
             return(
                 obj.author == request.user
                 or request.user.role in [request.user.ROLE_ADMIN,
