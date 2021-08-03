d94fb22dba1ed7649faac7fa0d0e5cbc18eaef20
AlekseiSandakov <axe455990@mail.ru>
2021-08-04 00:57:15 +0700

    my 45 commit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ environ.Env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'DEFAULT_PARAM')
 
 DEBUG = False
 
