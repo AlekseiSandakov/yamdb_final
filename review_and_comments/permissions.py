34899138a46e4ad41f592dda6ca2da49fef87cfd
AlekseiSandakov <axe455990@mail.ru>
2021-08-04 01:07:55 +0700

    my 46 commit

--- review_and_comments/permissions.py
+++ review_and_comments/permissions.py
@@ -1,6 +1,13 @@
 from rest_framework import permissions
 
 
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request):
-        return request.user.is_authenticated and request.user.is_admin
+class ReviewCommentPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return(
+                obj.author == request.user
+                or request.user.role in [request.user.ROLE_ADMIN,
+                                         request.user.ROLE_MODERATOR]
+            )
